38e4f68929e3da69c084d039456c79be79feecb1
Ranjan <ranjankumarjha470@gmail.com>
2021-05-17 15:08:23 +0530

    intro js

--- Intro.js
+++ Intro.js
@@ -1,1 +1,18 @@
-console.log("Hello World! ");
+// console.log("Hello World! ");
+let data = {
+ name: "Ranjan",
+ place : "Delhi",
+ movies : { 
+  name: "Captain america",
+  rating : 10,
+ }
+
+}
+// console.log(data.name);
+// let key = "place";
+// console.log(data[key]);
+// data.dob = "04-10-2000"; 
+// console.log(data);
+
+console.log(data.name);
+
